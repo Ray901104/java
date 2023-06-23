a151a54662cc70075a1e823cbcca9a4b6896c243
Youngrae Cho <ip323@naver.com>
2023-06-23 22:32:01 +0900

    실습한 JDBC 코드 리팩토링 및 DB 커넥션 풀 적용

--- src/main/java/org/example/jdbc/ConnectionManager.java
+++ src/main/java/org/example/jdbc/ConnectionManager.java
@@ -3,17 +3,46 @@ package org.example.jdbc;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionManager
 {
-    public static DataSource getDataSource()
+    private static final String DB_DRIVER = "org.h2.Driver";
+    public static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    public static final int MAX_POOL_SIZE = 40;
+    public static final String SA = "sa";
+    public static final String PASSWORD = "";
+
+    private static final DataSource ds;
+
+    static
     {
         HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
-        hikariDataSource.setUsername("sa");
-        hikariDataSource.setPassword("");
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
+        hikariDataSource.setUsername(SA);
+        hikariDataSource.setPassword(PASSWORD);
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
 
-        return hikariDataSource;
+        ds = hikariDataSource;
+    }
+
+    public static Connection getConnection()
+    {
+        try
+        {
+            return ds.getConnection();
+        }
+        catch (SQLException se)
+        {
+            throw new IllegalStateException(se);
+        }
+    }
+
+    public static DataSource getDataSource()
+    {
+        return ds;
     }
 }
--- src/main/java/org/example/jdbc/UserDao.java
+++ src/main/java/org/example/jdbc/UserDao.java
@@ -1,80 +1,29 @@
 package org.example.jdbc;
 
-import java.sql.*;
+import java.sql.SQLException;
 
-public class UserDao
-{
-    public void create(User user) throws SQLException
-    {
-        Connection con = null;
-        PreparedStatement pstmt = null;
+public class UserDao {
+    public void create(User user) throws SQLException {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
 
-        try
-        {
-            con = getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
+        jdbcTemplate.executeUpdate(user, sql, pstmt -> {
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getName());
             pstmt.setString(4, user.getEmail());
-
-            pstmt.executeUpdate();
-        }
-        catch (SQLException | ClassNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            if (pstmt != null) { pstmt.close(); }
-
-            if (con != null) { con.close(); }
-        }
+        });
     }
 
-    public User findByUserId(String userId) throws SQLException, ClassNotFoundException
-    {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try
-        {
-            con = getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userId = ?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next())
-            {
-                user = new User(
-                        rs.getString("userId"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("email"));
-            }
-            return user;
-        }
-        finally
-        {
-            if (rs != null) { rs.close(); }
-            if (pstmt != null) { pstmt.close(); }
-            if (con != null) { con.close(); }
-
-        }
-    }
-
-    private Connection getConnection() throws SQLException, ClassNotFoundException
-    {
-        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
-        String id = "sa";
-        String pw = "";
-
-        Class.forName("org.h2.Driver");
-        return DriverManager.getConnection(url, id, pw);
+    public User findByUserId(String userId) throws SQLException, ClassNotFoundException {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "SELECT userId, password, name, email FROM USERS WHERE userId = ?";
+        return (User) jdbcTemplate.executeQuery(sql,
+                pstmt -> pstmt.setString(1, userId),
+                resultSet -> new User(
+                    resultSet.getString("userId"),
+                    resultSet.getString("password"),
+                    resultSet.getString("name"),
+                    resultSet.getString("email")));
     }
 }
