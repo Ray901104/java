cde307d0411fca36a0747656603268582b2f3dcf
Youngrae Cho <ip323@naver.com>
2023-06-29 22:03:05 +0900

    DI 프레임워크 구현

---
+++ src/main/java/org/example/di/di/BeanFactory.java
@@ -0,0 +1,80 @@
+package org.example.di.di;
+
+import org.example.di.BeanFactoryUtils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+public class BeanFactory
+{
+    private final Set<Class<?>> preInstantiatedClazz;
+    private Map<Class<?>, Object> beans = new HashMap<>();
+
+    public BeanFactory(Set<Class<?>> preInstantiatedClazz)
+    {
+        this.preInstantiatedClazz = preInstantiatedClazz;
+        initialize();
+    }
+
+    private void initialize()
+    {
+        for (Class<?> clazz : preInstantiatedClazz)
+        {
+            Object instance = createdInstance(clazz);
+            beans.put(clazz, instance);
+        }
+    }
+
+    private Object createdInstance(Class<?> clazz)
+    {
+        // 생성자
+        Constructor<?> constructor = findConstructor(clazz);
+
+        // 파라미터
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> typeClass : constructor.getParameterTypes())
+        {
+            parameters.add(getParameterByClass(typeClass));
+        }
+
+        // 인스턴스 생성
+        try
+        {
+            return constructor.newInstance(parameters.toArray());
+        }
+        catch (InstantiationException | IllegalAccessException | InvocationTargetException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Constructor<?> findConstructor(Class<?> clazz)
+    {
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+
+        if (Objects.nonNull(injectedConstructor))
+        {
+            return injectedConstructor;
+        }
+
+        return clazz.getConstructors()[0];
+    }
+
+    private Object getParameterByClass(Class<?> typeClass)
+    {
+        Object instanceBean = getBean(typeClass);
+
+        if (Objects.nonNull(instanceBean))
+        {
+            return instanceBean;
+        }
+
+        return createdInstance(typeClass);
+    }
+
+    public <T> T getBean(Class<T> requiredType)
+    {
+        return (T) beans.get(requiredType);
+    }
+}
